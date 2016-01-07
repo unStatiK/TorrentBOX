a9299173480b4861e5b34e3fb4983f047a2c4719
Steven Vch <unstatik@staremax.com>
2016-01-07 23:31:09 +0300

    implement generate account cli tool

---
+++ generate_account.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from utils import generate_password_hash
+import getopt
+import sys
+
+
+def show_help():
+    print "command options:"
+    print "-h show this help"
+    print "-n <name> : set name of account"
+    print "-p <password> : set password of account"
+    print "-s <status> : set status of account (status string should be admin or user), default value is admin [this option not required]"
+
+
+def show_args_error():
+    print "args -n and -p is required"
+
+
+def show_query(name, hash_str, status):
+    hash_value = "".join(["hash: ", hash_str])
+    account_insert_query = "INSERT INTO accounts (id, name, password, status) VALUES(default,'{0}','{1}',{2});" \
+        .format(name, hash_str, status)
+
+    print hash_value
+    print account_insert_query
+
+
+def generate_account():
+    try:
+        name = None
+        password = None
+        status = 2
+        need_help = False
+        opts, args = getopt.getopt(sys.argv[1:], "n:p:s:h")
+        if not opts:
+            show_args_error()
+        for key, value in opts:
+            if key == "-n":
+                name = value
+            if key == "-p":
+                password = value
+            if key == "-s":
+                if value == "admin":
+                    status = 2
+                if value == "user":
+                    status = 1
+            if key == "-h":
+                need_help = True
+
+        if need_help:
+            show_help()
+            return
+        if name and password:
+            hash_str = generate_password_hash(password)
+            show_query(name, hash_str, status)
+        else:
+            show_args_error()
+    except getopt.GetoptError as err:
+        print str(err)
+
+
+generate_account()
--- sql/create_admin.sql
+++
@@ -1,4 +0,0 @@
-/* password : admin */
-/* SALT_PASS your_salt_for_password */
-
-INSERT INTO accounts (id, name, password, status) VALUES(default,'admin','d033e22ae348aeb5660fc2140aec35850c4da997',2);
