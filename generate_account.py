0d26bf9b592e8d3fff1043d211f1f31fbfa0a91c
Steven Vch <unstatik@staremax.com>
2016-01-09 04:31:49 +0300

    add show hash in separate line option

--- generate_account.py
+++ generate_account.py
@@ -11,18 +11,22 @@ def show_help():
     print "-n <name> : set name of account"
     print "-p <password> : set password of account"
     print "-s <status> : set status of account (status string should be admin or user), default value is admin [this option not required]"
+    print "-v show generated hash string at separate line"
 
 
 def show_args_error():
     print "args -n and -p is required"
 
 
-def show_query(name, hash_str, status):
+def show_hash_string(hash_str):
     hash_value = "".join(["hash: ", hash_str])
+    print hash_value
+
+
+def show_query(name, hash_str, status):
     account_insert_query = "INSERT INTO accounts (id, name, password, status) VALUES(default,'{0}','{1}',{2});" \
         .format(name, hash_str, status)
 
-    print hash_value
     print account_insert_query
 
 
@@ -32,7 +36,8 @@ def generate_account():
         password = None
         status = 2
         need_help = False
-        opts, args = getopt.getopt(sys.argv[1:], "n:p:s:h")
+        need_show_hash_string = False
+        opts, args = getopt.getopt(sys.argv[1:], "n:p:s:hv")
         if not opts:
             show_args_error()
         for key, value in opts:
@@ -47,12 +52,16 @@ def generate_account():
                     status = 1
             if key == "-h":
                 need_help = True
+            if key == "-v":
+                need_show_hash_string = True
 
         if need_help:
             show_help()
             return
         if name and password:
             hash_str = generate_password_hash(password)
+            if need_show_hash_string:
+                show_hash_string(hash_str)
             show_query(name, hash_str, status)
         else:
             show_args_error()
